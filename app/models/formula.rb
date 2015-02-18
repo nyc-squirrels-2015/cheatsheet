8ada85d862cc0ab363a814b805d5cfe254ee34f0
derrickwu <derrickwu89>
2015-02-18 09:47:24 -0500

    Add model and migration

---
+++ app/models/formula.rb
@@ -0,0 +1,4 @@
+class Formula < ActiveRecord::Base
+  belongs_to :category
+  belongs_to :user
+end
---
+++ db/migrate/20150218093757_create_formulas.rb
@@ -0,0 +1,12 @@
+class CreateFormula < ActiveRecord::Migration
+  def change
+    create_table :formulas do |t|
+      t.string :name
+      t.string :content
+      t.references :user
+      t.references :category
+
+      t.timestamps
+    end
+  end
+end
