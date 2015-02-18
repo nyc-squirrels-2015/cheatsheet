afd11744d6487d64389a4ac19925b53ccd37fa25
derrickwu <derrickwu89>
2015-02-18 12:05:34 -0500

    Add category complete

--- app/controllers/catergory.rb
+++ app/controllers/catergory.rb
@@ -4,6 +4,13 @@ get "/category/:id" do
   erb :category
 end
 
-get "/profile/<%= @user.id%>/new_category" do
+get "/profile/:id/new_category" do
+  @user = User.find(params[:id])
   erb :category_form
+end
+
+post "/profile/:id/new_category" do
+  Category.create(name: params[:name], user_id: params[:id])
+  @user=User.find(params[:id])
+  redirect "/profile/#{@user.id}"
 end
